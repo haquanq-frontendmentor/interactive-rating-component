597b26217625e055ed78110d8206266f26d3a310
Ha Quanq <quangha0120010@gmail.com>
2025-07-07 21:58:34 +0700

    Add .editorconfig, refromat files

--- app/main.js
+++ app/main.js
@@ -2,15 +2,11 @@ const ratingCard = document.querySelector(".rating__wrapper");
 const successCard = document.querySelector(".appreciate__wrapper");
 const selectedRatingText = document.querySelector("#selected-rating-text");
 
-document
-  .querySelector("#interactive-rating-form")
-  .addEventListener("submit", (e) => {
+document.querySelector("#interactive-rating-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    const selectedValue = document.querySelector(
-      'input[name="rating"]:checked'
-    ).value;
+    const selectedValue = document.querySelector('input[name="rating"]:checked').value;
 
     selectedRatingText.textContent = selectedValue;
     ratingCard.style.display = "none";
     successCard.style.display = "flex";
-  });
+});
